bfe5a4df93c1c27057b06d748171155bed031c4e
Alex Schacher <alex_schacher@hotmail.com>
2020-12-04 23:37:32 -0600

    Camera Radius Light, Torch Flicker, Day Timer

--- Assets/Scripts/Lights/DayTimer.cs
+++ Assets/Scripts/Lights/DayTimer.cs
@@ -4,15 +4,106 @@ using UnityEngine;
 
 public class DayTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public enum PartOfDay { Morning, Day, Evening, Night }
+    public enum Season { Spring, Summer, Fall, Winter }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Material material;
+
+    [Header("Time")]
+    [SerializeField] private float timeSpeed = 1f;
+    [SerializeField] private float currentHour = 12f;
+    [SerializeField] private PartOfDay currentPartOfDay = PartOfDay.Day;
+    [SerializeField] private float currentLightValue = 1f;
+    [SerializeField] private Color currentColor = Color.white;
+
+    [Header("Tint")]
+    [SerializeField] private Color morningTint = Color.yellow;
+    [SerializeField] private Color dayTint = Color.white;
+    [SerializeField] private Color eveningTint = Color.red;
+    [SerializeField] private Color nightTint = Color.blue;
+
+    [Header("Daylight")]
+    [SerializeField] private float morningLightValue = 0.9f;
+    [SerializeField] private float dayLightValue = 1f;
+    [SerializeField] private float eveningLightValue = 0.6f;
+    [SerializeField] private float nightLightValue = 0f;
+
+    [Header("TransitionTime")]
+    [SerializeField] private float morningHour = 5f;
+    [SerializeField] private float dayHour = 8f;
+    [SerializeField] private float eveningHour = 17f;
+    [SerializeField] private float nightHour = 22f;
+
+    [Header("Transition")]
+    [SerializeField] private float transitionStartLightValue;
+    [SerializeField] private float transitionTargetLightValue;
+    [SerializeField] private Color transitionStartColor;
+    [SerializeField] private Color transitionTargetColor;
+    [SerializeField] private float transitionTimer;
+    [SerializeField] private float transitionLength;
+
+    
+    private void Start()
     {
-        
+        SetShaderValues();
+    }
+    private void Update()
+    {
+        UpdateHour();
+        CheckForStartTransition();
+        LerpTransition();
+    }
+    private void UpdateHour()
+    {
+        currentHour += Time.deltaTime * timeSpeed;
+        if (currentHour >= 24f) currentHour = currentHour % 24f;
+    }
+    private void CheckForStartTransition()
+    {
+        switch(currentPartOfDay)
+        {
+            case (PartOfDay.Morning) :
+                if (currentHour > dayHour) StartTransition(PartOfDay.Day, dayTint, dayLightValue, 10f);
+                break;
+            case (PartOfDay.Day) :
+                if (currentHour > eveningHour) StartTransition(PartOfDay.Evening, eveningTint, eveningLightValue, 10f);
+                break;
+            case (PartOfDay.Evening) :
+                if (currentHour > nightHour) StartTransition(PartOfDay.Night, nightTint, nightLightValue, 10f);
+                break;
+            case (PartOfDay.Night) :
+                if (currentHour > morningHour && currentHour < nightHour) StartTransition(PartOfDay.Morning, morningTint, morningLightValue, 10f);
+                break;
+            default: break;
+        }
+    }
+    private void StartTransition(PartOfDay newPartOfDay, Color newColor, float newLightValue, float length)
+    {
+        currentPartOfDay = newPartOfDay;
+        transitionStartLightValue = currentLightValue;
+        transitionStartColor = currentColor;
+        transitionTargetLightValue = newLightValue;
+        transitionTargetColor = newColor;
+        transitionLength = length;
+        transitionTimer = 0f;
+    }
+    private void LerpTransition()
+    {
+        if (transitionTimer > transitionLength) return;
+        transitionTimer += Time.deltaTime;
+
+        float lerpValue = transitionTimer / transitionLength;
+        currentLightValue = Mathf.Lerp(transitionStartLightValue, transitionTargetLightValue, lerpValue);
+        currentColor = new Color(
+            Mathf.Lerp(transitionStartColor.r, transitionTargetColor.r, lerpValue),
+            Mathf.Lerp(transitionStartColor.g, transitionTargetColor.g, lerpValue),
+            Mathf.Lerp(transitionStartColor.b, transitionTargetColor.b, lerpValue));
+
+        SetShaderValues();
+    }
+    private void SetShaderValues()
+    {
+        material.SetFloat("_Daylight", currentLightValue);
+        material.SetColor("_AtmosphereTint", currentColor);
     }
 }
--- Assets/Scripts/Lights/Torch.cs
+++ Assets/Scripts/Lights/Torch.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject lightObject;
+    private Light lightSource;
+    [SerializeField] private float radiusVariance = 0.1f;
+    [SerializeField] private float offsetVariance = 0.1f;
+    [SerializeField] private float changeRate = 0.2f;
+    private float changeTimer;
+    private Vector3 startPosition;
+    private float startRadius;
+
+    private void Awake()
     {
-        
+        startPosition = lightObject.transform.position;
+        lightSource = lightObject.GetComponent<Light>();
+        startRadius = lightSource.range;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        changeTimer += Time.deltaTime;
+        if (changeTimer > changeRate)
+        {
+            changeTimer = changeTimer % changeRate;
+            lightSource.range = startRadius + Random.Range(-radiusVariance, radiusVariance);
+            lightObject.transform.position = startPosition + new Vector3(
+                Random.Range(-offsetVariance, offsetVariance),
+                Random.Range(-offsetVariance, offsetVariance),
+                Random.Range(-offsetVariance, offsetVariance));
+        }
     }
 }
