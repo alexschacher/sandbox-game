2893aef23ce16f2eacd409c9c201627df321bfd8
Alex Schacher <alex_schacher@hotmail.com>
2020-12-21 21:20:27 -0600

    Level Editing Cursor

---
+++ Assets/Scripts/Level/BlockBitmask.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBitmask : MonoBehaviour
+{
+    // BlockBitmask should be a more generic class used to bitmask blocks.
+    // Maybe a blockBitmask scriptableObject input that records what meshes and rotations should be applied
+    // Probably relies on a leveldata array that needs to be set up, which relies on tiledata... etc
+
+
+    
+    /*
+    public void UpdateModel()
+    {
+        if (objectData == null) { return; }
+
+        Tile.ID[,] neighbors = worldData.GetNeighbors(objectData.GetX(), objectData.GetZ(), objectData.GetY());
+
+        int bitMask = 0;
+        if (Tile.IsBlock(neighbors[1, 0])) bitMask += 1;
+        if (Tile.IsBlock(neighbors[0, 1])) bitMask += 2;
+        if (Tile.IsBlock(neighbors[2, 1])) bitMask += 4;
+        if (Tile.IsBlock(neighbors[1, 2])) bitMask += 8;
+
+        switch(bitMask)
+        {
+            case  0: meshFilter.mesh = Resource.mesh["Block_RoundLone"];   transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            case  1: meshFilter.mesh = Resource.mesh["Block_RoundEnd"];    transform.rotation = Quaternion.Euler(0, -90, 0);   break;
+            case  2: meshFilter.mesh = Resource.mesh["Block_RoundEnd"];    transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            case  3: meshFilter.mesh = Resource.mesh["Block_RoundCorner"]; transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            case  4: meshFilter.mesh = Resource.mesh["Block_RoundEnd"];    transform.rotation = Quaternion.Euler(0, 180, 0);   break;
+            case  5: meshFilter.mesh = Resource.mesh["Block_RoundCorner"]; transform.rotation = Quaternion.Euler(0, -90, 0);   break;
+            case  6: meshFilter.mesh = Resource.mesh["Block_Lane"];        transform.rotation = Quaternion.Euler(0, 90, 0);    break;
+            case  7: meshFilter.mesh = Resource.mesh["Block_Edge"];        transform.rotation = Quaternion.Euler(0, -90, 0);   break;
+            case  8: meshFilter.mesh = Resource.mesh["Block_RoundEnd"];    transform.rotation = Quaternion.Euler(0, 90, 0);    break;
+            case  9: meshFilter.mesh = Resource.mesh["Block_Lane"];        transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            case 10: meshFilter.mesh = Resource.mesh["Block_RoundCorner"]; transform.rotation = Quaternion.Euler(0, 90, 0);    break;
+            case 11: meshFilter.mesh = Resource.mesh["Block_Edge"];        transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            case 12: meshFilter.mesh = Resource.mesh["Block_RoundCorner"]; transform.rotation = Quaternion.Euler(0, 180, 0);   break;
+            case 13: meshFilter.mesh = Resource.mesh["Block_Edge"];        transform.rotation = Quaternion.Euler(0, 180, 0);   break;
+            case 14: meshFilter.mesh = Resource.mesh["Block_Edge"];        transform.rotation = Quaternion.Euler(0, 90, 0);    break;
+            case 15: meshFilter.mesh = Resource.mesh["Block_Mid"];         transform.rotation = Quaternion.Euler(0, 0, 0);     break;
+            default: return;
+        }
+    }
+
+    private void UpdateNeighbors()
+    {
+        GameObject[,] neighbors = objectData.GetWorldObjectContainer().GetNeighbors(objectData.GetX(), objectData.GetZ(), objectData.GetY());
+
+        for (int ix = 0; ix <3; ix++)
+        {
+            for (int iz = 0; iz < 3; iz++)
+            {
+                if (neighbors[ix, iz] != null)
+                {
+                    if (neighbors[ix, iz].GetComponent<Block>() != null)
+                    {
+                        neighbors[ix, iz].GetComponent<Block>().UpdateModel();
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        UpdateNeighbors();
+    }
+    */
+}
---
+++ Assets/Scripts/Level/EditCursor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EditCursor : MonoBehaviour
+{
+    [SerializeField] private Level level;
+
+    [SerializeField] private InputActionAsset inputAsset;
+    private InputAction scrollAction, mousePositionAction;
+
+    [SerializeField] private int maxHeight;
+    private int cursorHeight;
+    private Vector3Int cursorPosition;
+
+    private void Awake()
+    {
+        InputActionMap inputActions = inputAsset.FindActionMap("Play");
+
+        scrollAction = inputActions.FindAction("Scroll");
+        scrollAction.performed += Scroll;
+        scrollAction.Enable();
+
+        mousePositionAction = inputActions.FindAction("MousePosition");
+        mousePositionAction.Enable();
+    }
+
+    private void Scroll(InputAction.CallbackContext context)
+    {
+        float scroll = context.ReadValue<float>();
+
+        if (scroll < 0 && transform.position.y < (maxHeight - 1) * 0.5f)
+        {
+            cursorHeight++;
+        }
+        if (scroll > 0 && transform.position.y > 0)
+        {
+            cursorHeight--;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateCursorPosition();
+    }
+
+    private void UpdateCursorPosition()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePositionAction.ReadValue<Vector2>());
+        Plane plane = new Plane(Vector3.up, -cursorHeight * 0.5f);
+        float distanceToPlane;
+        plane.Raycast(mouseRay, out distanceToPlane);
+
+       cursorPosition = new Vector3Int(
+            Mathf.RoundToInt(mouseRay.GetPoint(distanceToPlane).x),
+            cursorHeight,
+            Mathf.RoundToInt(mouseRay.GetPoint(distanceToPlane).z));
+
+        transform.position = new Vector3(cursorPosition.x, cursorPosition.y * 0.5f, cursorPosition.z);
+    }
+
+    private void OnPlaceObject()
+    {
+        if (IfWithinChunk() == false) return;
+
+        level.ModifyCell(ID.Ground, cursorPosition.x, cursorPosition.y, cursorPosition.z);
+    }
+
+    private void OnDeleteObject()
+    {
+        if (IfWithinChunk() == false) return;
+
+        level.ModifyCell(ID.Empty, cursorPosition.x, cursorPosition.y, cursorPosition.z);
+    }
+
+    private bool IfWithinChunk()
+    {
+        if (cursorPosition.x >= 0 &&
+            cursorPosition.z >= 0 &&
+            cursorPosition.x < 8 &&
+            cursorPosition.z < 8)
+        {
+            return true;
+        }
+        return false;
+    }
+}
